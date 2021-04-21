889c75e0a3c6e43aa9d9da48f712d54584b524a1
Li Wen <1625772187@qq.com>
2021-04-21 23:23:49 +0800

    Li Wen,20210421

--- main.java
+++ main.java
@@ -1,6 +1,5 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Date;
 
 /*
 created by Yuxin Zhu in 2021/03/21
@@ -8,8 +7,6 @@ class:主函数
  */
 public class main {
     private static AllUser Users;//记得初始化
-
-    private AllUser users;
 
     public static void main(String[] args) {
         LoginMenu Login = new LoginMenu();
@@ -24,7 +21,6 @@ public class main {
         });
 
         Login.show();
-
     }
 }
 
